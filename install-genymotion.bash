c4a0fb8fe4a4f951cd1e39c9e1bb1ca390c62716
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-06-15 14:35:44 +0700

    yes

--- install-genymotion.bash
+++ install-genymotion.bash
@@ -4,4 +4,4 @@ sudo apt-get -y install virtualbox
 cd ~
 sudo wget --output-document=genymotion.bin https://dl.genymotion.com/releases/genymotion-3.4.0/genymotion-3.4.0-linux_x64.bin
 sudo chmod +x ~/genymotion.bin
-sudo yes | sudo ~/genymotion.bin
+yes | sudo ~/genymotion.bin
--- install-github-desktop.bash
+++ install-github-desktop.bash
@@ -3,4 +3,4 @@
 cd ~
 sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.2.1-linux1/GitHubDesktop-linux-3.2.1-linux1.deb
 sudo apt-get install gdebi-core 
-sudo gdebi GitHubDesktop-linux-3.2.1-linux1.deb
+yes | sudo gdebi GitHubDesktop-linux-3.2.1-linux1.deb
