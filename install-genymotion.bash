8dc2b3324cf0a705fc2bee659b6d84eff7655c42
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-06-15 18:39:03 +0700

    x

--- install-genymotion.bash
+++ install-genymotion.bash
@@ -2,6 +2,7 @@ sudo apt-get --quiet update --yes
 sudo apt-get -y install virtualbox
 
 cd ~
+
 if [ -f ~/genymotion.bin ]; then
     echo "File exists."
 else 
@@ -9,5 +10,5 @@ else
     wget --output-document=genymotion.bin https://dl.genymotion.com/releases/genymotion-3.4.0/genymotion-3.4.0-linux_x64.bin
 
 sudo chmod +x ~/genymotion.bin
-yes | sudo ./genymotion.bin;
+yes | sudo ./genymotion.bin
 
