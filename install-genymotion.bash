9510abede085ba7a781935b46e385f61cdc802a1
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-06-15 18:37:25 +0700

    a

--- install-genymotion.bash
+++ install-genymotion.bash
@@ -6,7 +6,7 @@ if [ -f ~/genymotion.bin ]; then
     echo "File exists."
 else 
     echo "File does not exist."
-    sudo wget --output-document=genymotion.bin https://dl.genymotion.com/releases/genymotion-3.4.0/genymotion-3.4.0-linux_x64.bin
+    wget --output-document=genymotion.bin https://dl.genymotion.com/releases/genymotion-3.4.0/genymotion-3.4.0-linux_x64.bin
 
 sudo chmod +x ~/genymotion.bin
 yes | sudo ~/genymotion.bin
