e5ceb650947884170e6178805fa71aa9d8c0ef9f
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-06-15 18:38:17 +0700

    xx

--- install-genymotion.bash
+++ install-genymotion.bash
@@ -9,4 +9,5 @@ else
     wget --output-document=genymotion.bin https://dl.genymotion.com/releases/genymotion-3.4.0/genymotion-3.4.0-linux_x64.bin
 
 sudo chmod +x ~/genymotion.bin
-yes | sudo ~/genymotion.bin
+yes | sudo ./genymotion.bin;
+
