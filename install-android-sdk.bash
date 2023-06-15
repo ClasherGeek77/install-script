32e3cc5af9f8e91e1034350d13bc9507745798fc
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-06-15 18:11:21 +0700

    echo "export

--- install-android-sdk.bash
+++ install-android-sdk.bash
@@ -8,12 +8,12 @@ if [ -f ~/android-sdk.zip ]; then
     echo "File exists."
 else 
     echo "File does not exist."
-    sudo wget --output-document=android-sdk.zip https://dl.google.com/android/repository/commandlinetools-linux-6200805_latest.zip
+    wget --output-document=android-sdk.zip https://dl.google.com/android/repository/commandlinetools-linux-6200805_latest.zip
 fi
 sudo unzip -d $HOME/android-sdk-29 android-sdk.zip
-sudo echo y | android-sdk-29/tools/bin/sdkmanager "platform-tools" "platforms;android-29" >/dev/null
-sudo echo y | android-sdk-29/tools/bin/sdkmanager "build-tools;29.0.3" >/dev/null
-sudo echo "export ANDROID_HOME=$HOME/android-sdk-29" >> ~/.bashrc
-sudo echo "export PATH=$PATH:$ANDROID_HOME/platform-tools/" >> ~/.bashrc
+echo y | android-sdk-29/tools/bin/sdkmanager "platform-tools" "platforms;android-29" >/dev/null
+echo y | android-sdk-29/tools/bin/sdkmanager "build-tools;29.0.3" >/dev/null
+echo "export ANDROID_HOME=$HOME/android-sdk-29" >> ~/.bashrc
+echo "export PATH=$PATH:$ANDROID_HOME/platform-tools/" >> ~/.bashrc
 
 
