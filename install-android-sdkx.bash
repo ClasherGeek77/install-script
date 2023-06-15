b224f70b36d5c0b74e0c6a8e5cd65a6492bf79a0
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-06-15 18:20:14 +0700

    tes

--- install-android-sdkx.bash
+++ install-android-sdkx.bash
@@ -11,7 +11,7 @@ else
     wget --output-document=android-sdk.zip https://dl.google.com/android/repository/commandlinetools-linux-6200805_latest.zip
 fi
 sudo unzip -d $HOME/android-sdk-29 android-sdk.zip
-sudo echo y | android-sdk-29/tools/bin/sdkmanager "platform-tools" "platforms;android-29" >/dev/null
-sudo echo y | android-sdk-29/tools/bin/sdkmanager "build-tools;29.0.3" >/dev/null
 echo 'export ANDROID_HOME="$HOME/android-sdk-29"' >> ~/.bashrc
 echo 'export PATH="$PATH:$ANDROID_HOME/platform-tools/"' >> ~/.bashrc
+sudo echo y | android-sdk-29/tools/bin/sdkmanager "platform-tools" "platforms;android-29" >/dev/null
+sudo echo y | android-sdk-29/tools/bin/sdkmanager "build-tools;29.0.3" >/dev/null
