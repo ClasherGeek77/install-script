04ef1f6ace2c5b59a5d17362e44583e5b230cd13
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-06-15 17:29:36 +0700

    up

--- setup-python369.bash
+++ setup-python369.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo apt-get update; sudo apt-get install make build-essential gcc libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 if pyenv install 3.6.9 ; then
     echo "Install succeeded"
 else
