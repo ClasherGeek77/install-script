7380adbe65bc62b318b7af4ca327ba1200e1bcd9
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-06-15 17:30:48 +0700

    up

--- setup-python369.bash
+++ setup-python369.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
-sudo apt-get update; sudo apt-get install make build-essential gcc libssl-dev zlib1g-dev \
+sudo apt-get update
+yes | sudo apt-get install make build-essential gcc libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 if pyenv install 3.6.9 ; then
     echo "Install succeeded"
 else
     echo "Install Failed, Try another install"
-    sudo apt install clang -y
+    yes | sudo apt install clang -y
     CC=clang pyenv install 3.6.9
 fi
 pyenv global 3.6.9
