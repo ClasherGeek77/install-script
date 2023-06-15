a2d4fa265e66f18e66a1901e051b09f35214ce78
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-06-15 18:54:53 +0700

    x

--- install-adb.bash
+++ install-adb.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -f /usr/bin/adb ]; then
-    echo "File exists."
+    echo "File exists"
 else 
-    echo "File does not exist."
+    echo "File does not exist"
     sudo wget --output-document=/usr/bin/adb https://raw.githubusercontent.com/ClasherGeek77/install-script/main/adb41
 fi
 sudo chmod +x ~/.adb41
