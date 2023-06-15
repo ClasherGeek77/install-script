4f4b849ce8187adfd974b4d164538831b4cea977
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-06-15 15:11:57 +0700

    change

--- install-github-desktop.bash
+++ install-github-desktop.bash
@@ -5,7 +5,7 @@ if [ -f ~/GitHubDesktop-linux-3.2.0-linux1.deb ]; then
     echo "File exists."
 else 
     echo "File does not exist."
-    sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.2.1-linux1/GitHubDesktop-linux-3.2.0-linux1.deb
+    sudo wget https://github.com/shiftkey/desktop/releases/download/release-3.2.0-linux1/GitHubDesktop-linux-3.2.0-linux1.deb
 fi
 sudo apt-get install gdebi-core 
 yes | sudo gdebi GitHubDesktop-linux-3.2.0-linux1.deb
