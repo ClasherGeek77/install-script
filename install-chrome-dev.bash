248c66fd1acf912ea96acbedc4d36c591f5bcd9d
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-06-16 18:15:44 +0700

    da

--- install-chrome-dev.bash
+++ install-chrome-dev.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo add-apt-repository -y ppa:flatpak/stable
-sudo apt update
+sudo apt-get --quiet update --yes
 sudo apt -y install flatpak
 sudo apt -y install gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
