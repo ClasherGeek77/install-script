aeadde4898ce70cda7eb76d08a5c009e8d13426b
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-06-16 18:09:54 +0700

    asdasd

--- install-chrome-dev.bash
+++ install-chrome-dev.bash
@@ -4,7 +4,7 @@ sudo add-apt-repository -y ppa:flatpak/stable
 sudo apt update
 sudo apt -y install flatpak
 sudo apt -y install gnome-software-plugin-flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 exec "$SHELL"
 yes | flatpak install flathub com.google.ChromeDev
 flatpak run com.google.ChromeDev
