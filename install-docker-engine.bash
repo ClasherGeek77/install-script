d6fe615525ad46213455d03b56c6865ec6a20eea
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-07-09 03:27:52 +0700

    Update install-docker-engine.bash

--- install-docker-engine.bash
+++ install-docker-engine.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt-get --quiet update --yes
-exec "$SHELL"
+exec bash
 sudo apt-get -y install ca-certificates curl gnupg
-exec "$SHELL"
+exec bash
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg -y --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -12,18 +12,18 @@ echo \
   "$(. /etc/os-release && echo "$UBUNTU_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-exec "$SHELL"
+exec bash
 
 sudo apt-get --quiet update --yes
-exec "$SHELL"
+exec bash
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-exec "$SHELL"
+exec bash
 sudo apt-get -y install dbus-user-session
 
-exec "$SHELL"
+exec bash
 
 sudo apt-get --quiet update --yes
-exec "$SHELL"
+exec bash
 sudo apt-get -y install docker-ce-rootless-extras
+exec bash
 
-exec "$SHELL"
