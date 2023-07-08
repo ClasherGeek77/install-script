acaf4f5367a6461c198b727d7f95c3588c65ee7d
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-07-09 03:45:21 +0700

    Update install-dedicated-chrome.bash

--- install-dedicated-chrome.bash
+++ install-dedicated-chrome.bash
@@ -5,7 +5,7 @@ sudo apt -y --purge remove google-chrome-stable
 sudo apt-get -y install gdebi-core 
 
 if [ -f ~/Downloads/google-chrome-stable_109.0.5414.74-1_amd64.deb ]; then
-    sudo rm -rf ~/android-sdk.zip
+    sudo rm -rf ~/Downloads/google-chrome-stable_109.0.5414.74-1_amd64.deb
     echo "File exists. redownloading..."
     sudo wget -q --show-progress --output-document=$HOME/Downloads/google-chrome-stable_amd64.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_109.0.5414.74-1_amd64.deb
 else 
@@ -17,7 +17,7 @@ yes | sudo gdebi ~/Downloads/google-chrome-stable_amd64.deb
 
 
 if [ -f ~/Downloads/chromedriver_linux64.zip ]; then
-    sudo rm -rf ~/android-sdk.zip
+    sudo rm -rf ~/Downloads/google-chrome-stable_109.0.5414.74-1_amd64.deb
     echo "File exists. redownloading..."
     sudo wget -q --show-progress --output-document=$HOME/Downloads/chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/109.0.5414.74/chromedriver_linux64.zip
 else 
