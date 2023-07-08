4d54434eb490c81d4a5bde5af654ad73b76c0c3d
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-07-09 02:42:27 +0700

    Update install-github-desktop.bash

--- install-github-desktop.bash
+++ install-github-desktop.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd ~
-if [ -f ~/GitHubDesktop-linux-3.2.0-linux1.deb ]; then
+if [ -f ~/GitHubDesktop-linux1.deb ]; then
     echo "File exists.redownloading..."
-    sudo rm -rf ~/GitHubDesktop-linux-3.2.0-linux1.deb
-    sudo wget -q --show-progress https://github.com/shiftkey/desktop/releases/download/release-3.2.0-linux1/GitHubDesktop-linux-3.2.0-linux1.deb
+    sudo rm -rf ~/GitHubDesktop-linux1.deb
+    sudo wget -q --show-progress --output-document=$HOME/GitHubDesktop-linux1.deb https://github.com/shiftkey/desktop/releases/download/release-3.2.5-linux1/GitHubDesktop-linux-3.2.5-linux1.deb
 else 
     echo "File does not exist."
-    sudo wget -q --show-progress https://github.com/shiftkey/desktop/releases/download/release-3.2.0-linux1/GitHubDesktop-linux-3.2.0-linux1.deb
+    sudo wget -q --show-progress --output-document=$HOME/GitHubDesktop-linux1.deb https://github.com/shiftkey/desktop/releases/download/release-3.2.5-linux1/GitHubDesktop-linux-3.2.5-linux1.deb
 fi
 sudo apt-get -y install gdebi-core 
-yes | sudo gdebi GitHubDesktop-linux-3.2.0-linux1.deb
+yes | sudo gdebi GitHubDesktop-linux1.deb
