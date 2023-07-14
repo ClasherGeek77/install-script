463a8e79da877a0fbbf821f4191249c8cb3a1977
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-07-14 18:45:11 +0700

    Update install-pyenv.bash

--- install-pyenv.bash
+++ install-pyenv.bash
@@ -4,9 +4,9 @@ sudo apt-get --quiet update --yes
 
 sudo apt-get -y install git
 
-sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+sudo apt-get install make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev --yes
 
 exec "$SHELL"
 source ~/.bashrc
