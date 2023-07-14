7aa6c66f7957e42891bc0d7d7e96e193fdc0ecf3
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-07-15 05:15:08 +0700

    xx

--- install-android-sdk.bash
+++ install-android-sdk.bash
@@ -22,7 +22,7 @@ else
     wget -q --show-progress --output-document=android-sdk.zip https://dl.google.com/android/repository/commandlinetools-linux-6200805_latest.zip
 fi
 
-sudo unzip -d -f $HOME/android-sdk-29 android-sdk.zip
+sudo unzip -f -d $HOME/android-sdk-29 android-sdk.zip
 exec "$SHELL"
 sudo chmod 777 $HOME/android-sdk-29
 exec "$SHELL"
