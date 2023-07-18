5b07cae843e07794f7ff895dfda44e92bfc9bd3c
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-07-18 19:03:31 +0700

    Update install-docker-engine.bash

--- install-docker-engine.bash
+++ install-docker-engine.bash
@@ -1,13 +1,16 @@
-
-sudo -s
 sudo apt-get --quiet update --yes
-exec "$SHELL"
+
 sudo apt-get -y install ca-certificates curl gnupg
-exec "$SHELL"
+
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo mkdir -p /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$UBUNTU_CODENAME")" stable" | \
@@ -15,19 +18,25 @@ echo \
 
 exec "$SHELL"
 
+sudo apt-get --quiet update --yes
+
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7EA0A9C3F273FCD8
+
 exec "$SHELL"
 
 sudo apt-get --quiet update --yes
+
 exec "$SHELL"
+
 sudo apt-get install docker-ce=5:20.10.24~3-0~ubuntu-jammy docker-ce-cli=5:20.10.24~3-0~ubuntu-jammy containerd.io docker-buildx-plugin docker-compose-plugin
+
 exec "$SHELL"
+
 sudo apt-get -y install dbus-user-session
 
 exec "$SHELL"
 
-sudo apt-get --quiet update --yes
-exec "$SHELL"
 sudo apt-get -y install docker-ce-rootless-extras
+
 exec "$SHELL"
 
