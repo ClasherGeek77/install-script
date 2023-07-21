a40f29c2c347953ed1354b9abfdfeaa0abf3ae51
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-07-21 20:36:20 +0700

    Update install-appium-and-appium-inspector.bash

--- install-appium-and-appium-inspector.bash
+++ install-appium-and-appium-inspector.bash
@@ -29,9 +29,9 @@ mkdir ~/appium-inspector
 
 cd ~/appium-inspector
 
-sudo wget -q --show-progress --output-document=Appium-Inspector-linux.AppImage https://github.com/appium/appium-inspector/releases/download/v2023.6.1/Appium-Inspector-linux-2023.6.1.AppImage
-sudo wget -q --show-progress --output-document=appiuminspector.png https://github.com/ClasherGeek77/install-script/blob/main/appiuminspector.png?raw=true
-sudo wget -q --show-progress --output-document=appium-inspector.desktop https://raw.githubusercontent.com/ClasherGeek77/install-script/main/appium-inspector.desktop
+wget -q --show-progress --output-document=Appium-Inspector-linux.AppImage https://github.com/appium/appium-inspector/releases/download/v2023.6.1/Appium-Inspector-linux-2023.6.1.AppImage
+wget -q --show-progress --output-document=appiuminspector.png https://github.com/ClasherGeek77/install-script/blob/main/appiuminspector.png?raw=true
+wget -q --show-progress --output-document=appium-inspector.desktop https://raw.githubusercontent.com/ClasherGeek77/install-script/main/appium-inspector.desktop
 
 sudo mv ~/appium-inspector/appium-inspector.desktop ~/.local/share/applications
 sudo mv ~/appium-inspector /opt/
