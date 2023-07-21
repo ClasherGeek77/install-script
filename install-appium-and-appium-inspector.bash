7cc2f8935c3f7ed628da4afdba326a3db93938c0
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2023-07-21 19:33:25 +0700

    Update install-appium-and-appium-inspector.bash

--- install-appium-and-appium-inspector.bash
+++ install-appium-and-appium-inspector.bash
@@ -7,15 +7,11 @@ sudo modprobe -v fuse
 sudo addgroup fuse
 sudo adduser $USER fuse
 
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt-get --quiet update --yes
 sudo apt-get -y install nodejs
 sudo apt-get -y install npm
 sudo chown -R $USER /usr/local
-exec "$SHELL"
-sudo npm install -g n
-exec "$SHELL"
-sudo n latest
-exec "$SHELL"
-sudo npm install -g npm@latest
 exec "$SHELL"
 sudo npm install -g appium@1.22.3
 exec "$SHELL"
@@ -27,13 +23,13 @@ then
     sudo rm -rf /opt/appium-inspector
     sudo rm -rf ~/.local/share/applications/appium-inspector.desktop
 fi
-mkdir appium-inspector
+mkdir ~/appium-inspector
 
-cd ~/appium-inspector
+cd ~/appium-inspector/
 
-sudo wget -q --show-progress --output-document=Appium-Inspector-linux.AppImage https://github.com/appium/appium-inspector/releases/download/v2023.6.1/Appium-Inspector-linux-2023.6.1.AppImage
-sudo wget -q --show-progress --output-document=appiuminspector.png https://github.com/ClasherGeek77/install-script/blob/main/appiuminspector.png?raw=true
-sudo wget -q --show-progress --output-document=appium-inspector.desktop https://raw.githubusercontent.com/ClasherGeek77/install-script/main/appium-inspector.desktop
+sudo wget -q --show-progress --output-document=~/appium-inspector/Appium-Inspector-linux.AppImage https://github.com/appium/appium-inspector/releases/download/v2023.6.1/Appium-Inspector-linux-2023.6.1.AppImage
+sudo wget -q --show-progress --output-document=~/appium-inspector/appiuminspector.png https://github.com/ClasherGeek77/install-script/blob/main/appiuminspector.png?raw=true
+sudo wget -q --show-progress --output-document=~/appium-inspector/appium-inspector.desktop https://raw.githubusercontent.com/ClasherGeek77/install-script/main/appium-inspector.desktop
 
 sudo chmod a+x ~/appium-inspector/Appium-Inspector-linux.AppImage
 
