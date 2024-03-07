9ee7d5d04dbfa5e3f2a09593d5dabcc58da7f2eb
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2024-03-07 15:11:48 +0700

    appium only install

---
+++ install-appium-only.bash
@@ -0,0 +1,18 @@
+
+sudo apt-get --quiet update --yes
+
+sudo apt remove nodejs npm -y
+curl -s https://deb.nodesource.com/setup_18.x | sudo bash
+sudo apt-get update --yes
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
+sudo chown -R $USER /usr/local
+exec "$SHELL"
+source ~/.bashrc
+sudo npm install --location=global appium
+sudo appium driver install --source=npm appium-uiautomator2-driver
+exec "$SHELL"
+source ~/.bashrc
+
+exec "$SHELL"
+source ~/.bashrc
