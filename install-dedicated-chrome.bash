cab4e6bc53b079e5a8eec01118606b3b53f20cd5
Teddy Lioner <52255311+ClasherGeek77@users.noreply.github.com>
2024-03-19 13:50:16 +0700

    Update install-dedicated-chrome.bash

--- install-dedicated-chrome.bash
+++ install-dedicated-chrome.bash
@@ -7,12 +7,10 @@ sudo apt-get -y install gdebi-core
 if [ -f ~/Downloads/google-chrome-stable_109.0.5414.74-1_amd64.deb ]; then
     sudo rm -rf ~/Downloads/google-chrome-stable_109.0.5414.74-1_amd64.deb
     echo "File exists. redownloading..."
-    sudo wget -q --show-progress --output-document=$HOME/Downloads/google-chrome-stable_amd64.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_109.0.5414.74-1_amd64.deb
+    sudo wget -q --show-progress --output-document=$HOME/Downloads/google-chrome-stable_amd64.deb https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_109.0.5414.74-1_amd64.deb
 else 
     echo "File does not exist."
-    sudo wget -q --show-progress --output-document=$HOME/Downloads/google-chrome-stable_amd64.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_109.0.5414.74-1_amd64.deb
-fi
-
+    sudo wget -q --show-progress --output-document=$HOME/Downloads/google-chrome-stable_amd64.deb https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_109.0.5414.74-1_amd64.deb
 yes | sudo gdebi ~/Downloads/google-chrome-stable_amd64.deb
 
 
